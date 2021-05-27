8cab9306666bfdae42d9be95258ba69c1435fb15
bertienschoors <bertienschoors@hotmail.com>
2021-05-27 14:44:22 +0200

    Template + routes

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.theme')
+@section('content')
+<div class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Our <em>Events</em></h2>
+            <span>Aliquam id urna imperdiet libero mollis hendrerit</span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/other-1-720x480.jpg" alt="">
+            <div class="down-content">
+              <h4>Weddings</h4>
+              <div style="margin-bottom:10px;">
+                <span> <sup>$</sup>1500.00 - <sup>$</sup>3500.00 </span>
+              </div>
+              <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+              <a href="book-table.html" class="filled-button">Book Now</a>
+            </div>
+          </div>
+          
+          <br>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/other-2-720x480.jpg" alt="">
+            <div class="down-content">
+              <h4>Birthdays</h4>
+              <div style="margin-bottom:10px;">
+                <span> <sup>$</sup>1500.00 - <sup>$</sup>3500.00 </span>
+              </div>
+              <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+              <a href="book-table.html" class="filled-button">Book Now</a>
+            </div>
+          </div>
+
+          <br>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/other-3-720x480.jpg" alt="">
+            <div class="down-content">
+              <h4>Anniversaries</h4>
+              <div style="margin-bottom:10px;">
+                <span> <sup>$</sup>1500.00 - <sup>$</sup>3500.00 </span>
+              </div>
+              <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+              <a href="book-table.html" class="filled-button">Book Now</a>
+            </div>
+          </div>
+
+          <br>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/winkelmandje.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.theme')
+@section('content')
+Winkelmandje works
+@endsection
