ad8bfaf7d6f96428eb6d5ee2ab9909eb37363127
TamaraMombeek <mombeek_tamara@hotmail.com>
2021-06-15 13:31:32 +0200

    add mollie

---
+++ app/Http/Controllers/MollieController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mollie\Laravel\Facades\Mollie;
+
+class MollieController extends Controller
+{
+    public function  __construct() {
+        Mollie::api()->setApiKey('test_FbVACj7UbsdkHtAUWnCnmSNGFWMuuA'); // your mollie test api key
+    }
+
+    /**
+     * Redirect the user to the Payment Gateway.
+     *
+     * @return Response
+     */
+    public function preparePayment()
+    {   
+        
+        $payment = Mollie::api()->payments()->create([
+        'amount' => [
+            'currency' => 'EUR', // Type of currency you want to send
+            'value' => '10.00', // You must send the correct number of decimals, thus we enforce the use of strings
+        ],
+        'description' => 'Bestelling bij Foodify', 
+        'redirectUrl' => route('payment.success'), // after the payment completion where you to redirect
+        ]);
+    
+        $payment = Mollie::api()->payments()->get($payment->id);
+    
+        // redirect customer to Mollie checkout page
+        return redirect($payment->getCheckoutUrl(), 303);
+    }
+
+    /**
+     * Page redirection after the successfull payment
+     *
+     * @return Response
+     */
+    public function paymentSuccess() {
+        //echo 'Wij hebben jouw betaling ontvangen';
+        $latest = \App\Models\Order::latest()->first();
+        $latest->payment = 1;
+        $latest->save();
+        //redirecten naar bedankt
+    }
+  
+}
